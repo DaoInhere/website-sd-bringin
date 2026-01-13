c18dc17a30c89b87b2649b0ea784eaa8e7c908f7
RonaldaBA <hatinthecrate@gmail.com>
2026-01-13 14:10:53 +0700

    tambah schedule factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
-use App\Models\Post;
+use App\Models\Schedule;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,6 +21,8 @@ class DatabaseSeeder extends Seeder
         // 3. Buat 10 Berita Palsu
         Post::factory(10)->create();
         // User::factory(10)->create();
+
+        Schedule::factory(10)->create();
 
         $this->call(UserSeeder::class);
         $this->call(ScheduleSeeder::class);
