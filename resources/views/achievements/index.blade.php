70ab3e446dcb5315b14c5a40e8a03ee757cecffc
Huda Abdul Majid <23080960039@student.walisongo.ac.id>
2026-01-30 13:49:13 +0700

    update admin bagian penghargaan prestasi siswa

--- resources/views/achievements/index.blade.php
+++ resources/views/achievements/index.blade.php
@@ -3,12 +3,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <div class="flex justify-between items-center mb-6">
-                        <h2 class="text-2xl font-bold">Data Prestasi Siswa</h2>
-                        <a href="{{ route('achievements.create') }}" class="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">
-                            + Tambah Prestasi
-                        </a>
-                    </div>
+                   <div class="mb-4">
+                    <a href="{{ route('achievements.create') }}" 
+                    style="background-color: #16a34a; color: white; padding: 10px 20px; border-radius: 5px; text-decoration: none; display: inline-block;">
+                        + Tambah Prestasi Baru
+                    </a>
+                </div>
 
                     @if(session('success'))
                         <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4">
